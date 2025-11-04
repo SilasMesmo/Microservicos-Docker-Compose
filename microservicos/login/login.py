44697301ea249d8cf0e3486946b77a2603cb750a
SilasMesmo <silascorpnac@gmail.com>
2025-11-04 10:00:21 -0300

    Update login.py

--- microservicos/login/login.py
+++ microservicos/login/login.py
@@ -2,21 +2,20 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-# Simulação de base de dados
+#simulação de base de dados
 USUARIOS_DB = {
     "admin": "12345",
     "joao": "senhafraca"
 }
 
-# NOVA ROTA: Rota raiz para mostrar o formulário
+#mostrar o formulário
 @app.route('/', methods=['GET'])
 def mostrar_formulario():
-    # Flask procura automaticamente por arquivos em "templates/"
     return render_template('login.html') 
 
 @app.route('/login', methods=['POST'])
 def fazer_login():
-    # ... (Seu código JSON existente continua aqui) ...
+    
     dados = request.get_json()
     usuario = dados.get('usuario')
     senha = dados.get('senha')
